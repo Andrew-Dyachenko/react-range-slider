a01aa675ee9d079920df8c4b275bc1e86648df14
Andrew-Dyachenko <north.inhale@gmail.com>
2018-11-19 17:46:18 +0300

    Move to create-react-library boilerplate

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import RangeGallery from 'react-range-gallery'
+
+export default class App extends Component {
+	render () {
+		return (
+			<div>
+				<RangeGallery text='Modern React component module' />
+			</div>
+		)
+	}
+}
